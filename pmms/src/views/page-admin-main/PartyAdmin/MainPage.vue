5de66c832292452c1da345ddcd492a34d992d86b
张智顺 <5611608+hbuzzs@user.noreply.gitee.com>
2020-01-16 20:28:43 +0800

    a

--- pmms/src/views/page-admin-main/PartyAdmin/MainPage.vue
+++ pmms/src/views/page-admin-main/PartyAdmin/MainPage.vue
@@ -4,11 +4,35 @@
     :data="tableData.slice((currentPage-1)*pageSize,currentPage*pageSize)"
     @selection-change="handleSelectionChange">
         <el-table-column type="selection"> </el-table-column>
-        <el-table-column label="日期">
-          <template slot-scope="scope">{{ scope.row.stuName }}</template>
+        <el-table-column label="姓名">
+          <template slot-scope="scope">
+            <el-button type="text" size="small" @click="browseDetails(scope.row.stuId)">
+              {{ scope.row.stuName}}
+            </el-button>
+            <!-- 弹出对话框 -->
+            <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+            <el-form :model="form">
+              <el-form-item label="活动名称" :label-width="formLabelWidth">
+                <el-input v-model="form.name" autocomplete="off"></el-input>
+              </el-form-item>
+              <el-form-item label="活动区域" :label-width="formLabelWidth">
+                <el-select v-model="form.region" placeholder="请选择活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+            </div>
+          </el-dialog>
+          </template>
         </el-table-column>
-        <el-table-column prop="stuName" label="姓名"> </el-table-column>
-        <el-table-column prop="stuSex" label="地址" show-overflow-tooltip> </el-table-column>
+        <el-table-column prop="stuSex" label="性别"> </el-table-column>
+        <el-table-column prop="majorName" label="专业"> </el-table-column>
+        <el-table-column prop="stuclassName" label="班级"> </el-table-column>
+        <el-table-column prop="levelName" label="级别" show-overflow-tooltip> </el-table-column>
     </el-table>
     
     <div style="margin-top:20px;float:center">
@@ -22,8 +46,8 @@
     </div>
 
     <div style="margin:20px;float:right">
-        <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
         <el-button @click="toggleSelection()">取消选择</el-button>
+        <el-button @click="toggleSelection()">导出到excel</el-button>
     </div>
   </div>
 </template>
@@ -38,7 +62,7 @@
     data() {
       return {
         // 设置每一页显示多少数据
-        pageSize: 3,
+        pageSize: 10,
         // 设置当前页
         currentPage: 1,
         // 区分点击不同支部
@@ -47,11 +71,23 @@
         multipleSelection: [],
         // 存储表格所有数据
         tableData: [],
+        // 弹出对话框
+        dialogFormVisible: false,
+        form: {
+          stuSex: '',
+          stuName: '',
+          stuOriginPlace: '',
+          majorName: '',
+          stuclassName: '',
+          stuPhoto: '',
+          stuContactInformation: '',
+          applyTime: [],
+        },
+        formLabelWidth: '120px'
       }
     },
 
     create: function() {
-      
     },
     mounted: function () {
       // 用$on事件来接收参数
@@ -59,7 +95,7 @@
         this.branchNo = data;
         console.log("branchNoPartyAdmin:::" + data);
         // 通过then方法获取promise对象
-        this.getRequest("admin/getAllParty").then(res=>{
+        this.getRequest("admin/getAllPartyByPartyBranch/"+data).then(res=>{
           // res为获得到的state里的数据
           this.tableData = [];
           for(var i = 0; i <res.length; i++){
@@ -104,8 +140,27 @@
         let result = await getAwait();
         //console.log(result); 
         return result;
+      },
+      // 点击学生查看详细信息
+      browseDetails(stuId){
+        this.dialogFormVisible = true;
+        // 通过then方法获取promise对象
+        this.getRequest("admin/getStudentByStuId/"+stuId).then(res=>{
+          // res为获得到的state里的数据 记录所有申请的申请时间
+          this.applyTime = [];
+          this.stuSex = res[0].stuSex ;
+          this.stuName = res[0].stuName ;
+          this.stuOriginPlace = res[0].stuOriginPlace ;
+          this.majorName = res[0].majorName ;
+          this.stuclassName = res[0].stuclassName ;
+          this.stuPhoto = res[0].stuPhoto ;
+          this.stuContactInformation = res[0].stuContactInformation ;
+          for(var i = 0; i <res.length; i++){
+            this.applyTime.push(res[i].applyTime);
+          }
+          console.log(this.applyTime);
+        })
       }
-
     }
   }
 </script>
